8f633abfeaea1104033a2128c6d1e0451b9a681d
Miho Tomic <miho.tomic8@gmail.com>
2017-04-13 01:37:40 +0200

    added image component

--- public/js/app.jsx
+++ public/js/app.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const image1 = 'https://instagram.fzag2-1.fna.fbcdn.net/t51.2885-15/e35/17881940_1779132032402163_6107306588534472704_n.jpg';
-const image2 = 'https://instagram.fzag2-1.fna.fbcdn.net/t51.2885-15/e35/17881479_1435608866498210_2389061363533086720_n.jpg';
-const element = <img src={image1} />;
+const url1 = 'https://instagram.fzag2-1.fna.fbcdn.net/t51.2885-15/e35/17881940_1779132032402163_6107306588534472704_n.jpg';
+const url2 = 'https://instagram.fzag2-1.fna.fbcdn.net/t51.2885-15/e35/17881479_1435608866498210_2389061363533086720_n.jpg';
+
+// function Image(props) {
+//     return <img src={props.url}/>;
+// }
+
+class Image extends React.Component {
+    render() {
+        return <img src={this.props.url}/>;
+    };
+}
 
 
 ReactDOM.render(
-    element,
+    <Image url={url1}/>,
     document.getElementById('root')
 );
