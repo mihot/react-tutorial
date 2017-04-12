11e5c2230e64c734109f048f869d0860115239ff
Miho Tomic <miho.tomic8@gmail.com>
2017-04-13 01:22:28 +0200

    added image gallery

--- public/js/app.jsx
+++ public/js/app.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const image1 = 'https://instagram.fzag2-1.fna.fbcdn.net/t51.2885-15/e35/17881940_1779132032402163_6107306588534472704_n.jpg';
+const image2 = 'https://instagram.fzag2-1.fna.fbcdn.net/t51.2885-15/e35/17881479_1435608866498210_2389061363533086720_n.jpg';
+const element = <img src={image1} />;
+
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    element,
     document.getElementById('root')
 );
