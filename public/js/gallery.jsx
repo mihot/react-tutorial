4c283461ea4310a165b1ca28d7d89d08e36bd3d2
Miho Tomic <miho.tomic8@gmail.com>
2017-04-13 09:17:51 +0200

    added data service

--- public/js/gallery.jsx
+++ public/js/gallery.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import Image from './image.jsx';
 
+import Data from './services/data.jsx';
+
 const urls = [
     'https://instagram.fzag2-1.fna.fbcdn.net/t51.2885-15/e35/17881940_1779132032402163_6107306588534472704_n.jpg',
     'https://instagram.fzag2-1.fna.fbcdn.net/t51.2885-15/e35/17881479_1435608866498210_2389061363533086720_n.jpg'
 ];
 
 class Gallery extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {urls};
+    }
     render() {
-        return <Image url={urls[0]}/>;
+        return (
+            <div>
+                {this.state.urls.map((url, index) =>
+                    <Image url={url} key={index}/>
+                )}
+            </div>
+        )
     };
 }
 
--- start.js
+++ start.js
@@ -10,6 +10,13 @@ app.set('views', path.join(__dirname, 'public', 'templates'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/images', (req, res) => {
+    res.json([
+        'https://instagram.fzag2-1.fna.fbcdn.net/t51.2885-15/e35/17881940_1779132032402163_6107306588534472704_n.jpg',
+        'https://instagram.fzag2-1.fna.fbcdn.net/t51.2885-15/e35/17881479_1435608866498210_2389061363533086720_n.jpg'
+    ]);
+});
+
 app.get('/*', (req, res) => {
     res.render('index');
 });
