eb9b7ea95495128075bd970dd1bfe69e231e0132
Miho Tomic <miho.tomic8@gmail.com>
2017-04-13 12:38:41 +0200

    finished tutorial

--- public/js/app.jsx
+++ public/js/app.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { browserHistory, Router, IndexRoute, Route, Link } from 'react-router';
+
 import Gallery from './gallery.jsx';
+import Text from './text.jsx';
+
+function App(props) {
+    return (
+        <div>
+            <h1><Link to="/text">Lidijaaaaaaaa</Link></h1>
+            {props.children}
+        </div>
+    )
+};
+
+
+const routes = {
+    // base component (wrapper for the whole application).
+    component: App,
+    childRoutes: [
+        {
+            path: '/',
+            component: Gallery
+        },
+        {
+            path: '/text',
+            component: Text
+        }
+    ]
+};
 
 
 ReactDOM.render(
-    <Gallery />,
+    <Router history={browserHistory} routes={routes} />,
     document.getElementById('root')
 );
---
+++ public/js/text.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Text extends React.Component {
+    render() {
+        return (
+            <p>
+                Blank page..........
+            </p>
+        );
+    };
+}
+
+export default Text;
